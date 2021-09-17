de76fc49bd48942798fd97ea4f31e3384fc9ca59
Tobias Kaatz <tkaatz@ibm.com>
2021-09-17 15:26:43 +0200

    feat: talk to db on localhost

--- backend.js
+++ backend.js
@@ -1,7 +1,8 @@
 const express = require('express');
 var mysql = require('mysql2'); 
 var con = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
+    //host: process.env.MYSQL_HOST,
+    host: "localhost",
     user: process.env.MYSQL_USER,
     password: process.env.MYSQL_PASSWORD
   });
